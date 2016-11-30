0097d9df3b78686e0abf85aefd5fcbcc30b82d45
idapenaflor <idapenaflor@gmail.com>
2016-11-30 19:40:25 +0800

    oits
    oi oi oi

--- signup.php
+++ signup.php
@@ -18,80 +18,69 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content-="width=device-width initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>MCL Room Reservation</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="./css/jquery.datetimepicker.css">
-    <link rel="stylesheet" href="./css/AdminLTE.min.css">
-    <link rel="stylesheet" href="./css/style.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
-    <script src="./js/dropdown.js"></script>
+    <?php include('includes.php'); ?>
   </head>
 
   <body>
-  	<!--======NAVIGATION===================-->
-  	<nav class="navbar navbar-default navbar-static-top" style="margin-bottom: 0px;">
-	  <div class="navbar-custom-menu">
-		  <div class="pull-right">
-		      <ul class="nav navbar-nav">
-		        <li class="dropdown user user-menu">
-		            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-		              <span class="glyphicon glyphicon-user"></span><span class="hidden-xs">&nbsp;<?php echo($lname.', '.$fname.' ('.$mname.')');?></span><span class="caret"></span>
-		            </a>
-		            <ul class="dropdown-menu">
-		              <!-- <li class="header">&nbsp;</li> -->
-		              <li role="separator" class="divider"></li>
-		              <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Logout</a></li>
-		            </ul>
-		        </li><!--end of li class user-->
-		      </ul>
-	      </div>
-    </div>
-	</nav>
+  	<header class="main-header">
+    <?php include ('header.php');?>
+    <?php include ('navi.php');?>
+ 	</header>
 
-	<div id="parentDiv">
-		 <div class="container">
-		 		<div class="row" style="margin-top: 40px;">
-					
-						<div class="panel" id="sign-up">
-								
-									<div class="input-group">
-									<form name="forms"  action="signup.php" method="POST">
-										<strong style="text-align: left; font-size: 25px;">Sign Up</strong>
-									  <input type="text" class="form-control" placeholder="Last Name" aria-describedby="basic-addon1" maxlength="50" name="txtLName" pattern="[A-Za-z\s]+" title="Enter letters only" required="required"> <br/><br/>
+ 	<aside class="main-sidebar">
+        <?php include('sidebar.php') ?>
+    </aside>
+    <div class="content-wrapper">
+    <section class="content-header">
+            <h1>
+              Register Account
+            </h1>
+            <ol class="breadcrumb">
+              <li class="active"><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            </ol>
+          </section>
+    <section class="content"> <hr/>
+            <div class="row">
+              <div class="col-xs-6">
+                <div class="box">
+                  <div class="box-body">							
+		<div class="input-group">
+		<form name="forms"  action="signup.php" method="POST">
+		  <input type="text" class="form-control" placeholder="Last Name" aria-describedby="basic-addon1" maxlength="50" name="txtLName" pattern="[A-Za-z\s]+" title="Enter letters only" required="required">
 
-									  <input type="text" class="form-control" placeholder="First Name" pattern="[A-Za-z\s]+" title="Enter letters only" required="required" aria-describedby="basic-addon1" maxlength="50" name="txtFName">
+		  <input type="text" class="form-control" placeholder="First Name" pattern="[A-Za-z\s]+" title="Enter letters only" required="required" aria-describedby="basic-addon1" maxlength="50" name="txtFName">
 
-									  <input type="text" class="form-control" placeholder="Middle Name" pattern="[A-Za-z\s]+" title="Enter letters only" required="required" aria-describedby="basic-addon1" maxlength="50" name="txtMName">
+		  <input type="text" class="form-control" placeholder="Middle Name" pattern="[A-Za-z\s]+" title="Enter letters only" required="required" aria-describedby="basic-addon1" maxlength="50" name="txtMName">
 
-									  <input type="text" class="form-control" placeholder="Account ID" required="required" aria-describedby="basic-addon1" maxlength="10" pattern="[0-9]{10}" title="Enter exactly 10 numbers only" name="txtUser">
+		  <input type="text" class="form-control" placeholder="Account ID" required="required" aria-describedby="basic-addon1" maxlength="10" pattern="[0-9]{10}" title="Enter exactly 10 numbers only" name="txtUser">
 
-									  <input type="password" class="form-control" placeholder="Password" required="required" aria-describedby="basic-addon1" id="txtPass" name="txtPass" pattern=".{6,}" title="Enter 6 or more characters">
+		  <input type="password" class="form-control" placeholder="Password" required="required" aria-describedby="basic-addon1" id="txtPass" name="txtPass" pattern=".{6,}" title="Enter 6 or more characters">
 
-									  <input type="password" class="form-control" placeholder="Confirm Password" required="required" pattern=".{6,}" title="Enter 6 or more characters" aria-describedby="basic-addon1" id="txtCPass" name="txtCPass">
+		  <input type="password" class="form-control" placeholder="Confirm Password" required="required" pattern=".{6,}" title="Enter 6 or more characters" aria-describedby="basic-addon1" id="txtCPass" name="txtCPass">
 
-									  <select class="form-control" name="uType" id="uType" required="required" aria-labelledby="dropdownMenu1" onchange="aDrop()">
-												<option value="">Enter user type</option>
-												<option value="Staff">Staff</option>
-												<option value="Dean">Dean</option>
-							          			<option value="CDMO" aria-labelledby="dropdownMenu1">CDMO</option>
-							          			<option value="LMO" aria-labelledby="dropdownMenu1">LMO</option>
-							          			<option value="OITS" aria-labelledby="dropdownMenu1">OITS</option>
-									  </select> <br/>
+		  <select class="form-control" name="uType" id="uType" required="required" aria-labelledby="dropdownMenu1" onchange="aDrop()">
+					<option value="">Enter user type</option>
+					<option value="Staff">Staff</option>
+					<option value="Dean">Dean</option>
+          			<option value="CDMO" aria-labelledby="dropdownMenu1">CDMO</option>
+          			<option value="LMO" aria-labelledby="dropdownMenu1">LMO</option>
+          			<option value="OITS" aria-labelledby="dropdownMenu1">OITS</option>
+		  </select> <br/>
 
-										<select class="form-control" name="Dept" id="Dept" required="required">
-											<option value="">Enter department</option>
-										</select>
+			<select class="form-control" name="Dept" id="Dept" required="required">
+				<option value="">Enter department</option>
+			</select>
 
-									<center><input type="submit" class="btn btn-primary" name="btnSubmit" value="Submit" style="padding-top: 10px; margin-top: 10px;"/></center>
-								</form>
-								</div><!--end of div-->
-						</div> <!--end of panel-->
-				</div>
-		
-		 </div><!--end of class container-->
+		<center><input type="submit" class="btn btn-primary" name="btnSubmit" value="Submit" style="padding-top: 10px; margin-top: 10px;"/></center>
+	</form>
+	</div>
+	</div>
+	</div>
+	</div>
+	</section>
 	</div>
 	<footer>© 2016 All Rights Reserved</footer>
       <script    src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
